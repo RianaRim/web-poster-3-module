2b8d009cf471ac831c08494389de7fef8c925df4
RianaRim <cheburashkanataburetke@gmail.com>
2026-03-03 20:41:07 +0300

    imageByCursor almost done

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,10 @@
 // движение кастомного курсора за движением мыши
 cursorMove()
 links()
+changeImageByCursor()
 
+
+// передвижение кастомного курсора
 function cursorMove(){
     const cursor = document.querySelector('.customCursor');
 
@@ -26,10 +29,59 @@ function links() {
            cursor.classList.remove('hoverCursor')
            console.log('mouseout')
          })
+
+         
     })
     
 }
 
+// изменение изображения по курсору
+function changeImageByCursor() {
+    let x = document.querySelector('.XCoord')
+    let y = document.querySelector('.YCoord')
+    let box = document.querySelector('.imageBox')
+    let section = document.querySelector('.changeImage')
+
+    document.addEventListener('mousemove', (event) => {
+        let cursor_x = event.pageX
+        let cursor_y = event.pageY
+
+        x.innerHTML = `X: ${cursor_x}`
+        y.innerHTML = `Y: ${cursor_y}`
+
+        let bodySizeX = document.documentElement.clientWidth
+        let bodySizeY = document.documentElement.clientHeight
+
+        let img1 = document.querySelector('.imageBox1')
+        let img2 = document.querySelector('.imageBox2')
+        let img3 = document.querySelector('.imageBox3')
+        let img4 = document.querySelector('.imageBox4')
+
+        if(cursor_x < bodySizeX && cursor_y < bodySizeY) {
+            img1.style.opacity = '1'
+        } else {
+            img1.style.opacity = '0'
+        }
+        
+        if (cursor_x > bodySizeX && cursor_y < bodySizeY) {
+          img2.style.opacity = '1'
+        } else {
+          img2.style.opacity = '0'
+        }
+
+        if (cursor_x > bodySizeX && cursor_y > bodySizeY) {
+          img3.style.opacity = '1'
+        } else {
+          img3.style.opacity = '0'
+        }
+
+        if (cursor_x < bodySizeX && cursor_y > bodySizeY) {
+          img4.style.opacity = '1'
+        } else {
+          img4.style.opacity = '0'
+        }
+    })
+}
 
 // анимация времени
 
